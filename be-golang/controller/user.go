7135f68a03d46782ca75edacd40e8ec730f3c8a0
Tu Phuong Danh <tuphuongdanh1996@gmail.com>
2023-05-17 12:01:15 +0700

    complete server side

--- be-golang/controller/user.go
+++ be-golang/controller/user.go
@@ -1,67 +1,86 @@
 package controller
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"io"
+	"os"
 
 	"grpc-demo-golang/protos"
+	"grpc-demo-golang/resource"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type User struct {
 	protos.UnimplementedUserServer
 }
 
-func (s *User) Get(ctx context.Context, in *protos.GetUserRequest) (*protos.GetUserResponse, error) {
-	// item, err := (&Service{}).Get(in.GetId())
-	// if err != nil {
-	// 	newErr := status.Errorf(codes.InvalidArgument, "Invalid argument")
-	// 	return nil, newErr
-	// }
+func (s *User) Get(ctx context.Context, request *protos.GetUserRequest) (*protos.GetUserResponse, error) {
+	user, err := resource.GetUser(request.GetId())
+	if err != nil {
+		newErr := status.Errorf(codes.NotFound, "Could not found user")
+		return nil, newErr
+	}
+
 	return &protos.GetUserResponse{
-		Data: &protos.UserMessage{
-			Name: "Tu Phuong Danh",
-		},
+		Data: marshalUser(user),
+	}, nil
+}
+
+func (s *User) Update(ctx context.Context, request *protos.UpdateUserRequest) (*protos.UpdateUserResponse, error) {
+	user, err := resource.UpdateUser(request.GetId(), request.GetName(), request.GetEmail())
+	if err != nil {
+		newErr := status.Errorf(codes.NotFound, "Could not found user")
+		return nil, newErr
+	}
+
+	return &protos.UpdateUserResponse{
+		Data: marshalUser(user),
 	}, nil
 }
 
 func (*User) Upload(stream protos.User_UploadServer) error {
-	// 	maxImageSize := 10000
-	// 	imageData := bytes.Buffer{}
-	// 	imageSize := 0
-	// 	var id int32
-	// 	for {
-	// 		req, err := stream.Recv()
-	// 		if err == io.EOF {
-	// 			goto END
-	// 		}
-	// 		if err != nil {
-	// 			return err
-	// 		}
-	// 		id = req.GetId()
-	// 		chunk := req.GetImage()
-	// 		size := len(chunk)
-	// 		imageSize += size
-	// 		if imageSize > maxImageSize {
-	// 			return fmt.Errorf("image is too large: %d > %d", imageSize, maxImageSize)
-	// 		}
-	// 		_, err = imageData.Write(chunk)
-	// 		if err != nil {
-	// 			return fmt.Errorf("cannot write chunk data: %v", err)
-	// 		}
-	// 	}
-	// END:
-	// 	fileName := "avatar_" + fmt.Sprint(id) + ".png"
-	// url, err := (&Service{}).SaveFile("./storage/uploads/"+fileName, imageData)
-	// if err != nil {
-	// 	return fmt.Errorf("Can not save the data: %v", err)
-	// }
-	// if _, saveErr := (&Service{}).Update(id, map[string]interface{}{"avatar": fileName}); saveErr != nil {
-	// 	return fmt.Errorf("Save failed: %v", saveErr)
-	// }
-	// err = stream.SendAndClose(&pb.UploadUserAvatarResponse{
-	// 	Url: *url,
-	// })
-	// if err != nil {
-	// 	return fmt.Errorf("cannot response data: %v", err)
-	// }
+	imageData := bytes.Buffer{}
+	var id int32
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			goto END
+		}
+		if err != nil {
+			return err
+		}
+		id = req.GetId()
+		chunk := req.GetImage()
+		_, err = imageData.Write(chunk)
+		if err != nil {
+			return fmt.Errorf("cannot write chunk data: %v", err)
+		}
+	}
+END:
+	fileName := "../database/storage/avatar_" + fmt.Sprint(id) + ".png"
+	file, err := os.Create(fileName)
+	if err != nil {
+		return fmt.Errorf("cannot create image file: %w", err)
+	}
+	_, err = imageData.WriteTo(file)
+	if err != nil {
+		return fmt.Errorf("cannot write image to file: %w", err)
+	}
+	stream.SendAndClose(&protos.UploadUserAvatarResponse{
+		Url: fileName,
+	})
 	return nil
+}
+
+func marshalUser(user *resource.UserModel) *protos.UserMessage {
+	return &protos.UserMessage{
+		Id:     user.ID,
+		Name:   user.Name,
+		Email:  user.Email,
+		Avatar: user.Avatar,
+	}
 }
---
+++ be-golang/resource/user.go
@@ -0,0 +1,99 @@
+package resource
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+const PATH_DATA = "/Users/blubberbee/workspace/own/demo/gRPC/database/data.json"
+
+type UserModel struct {
+	ID     int32
+	Name   string
+	Email  string
+	Avatar string
+}
+
+type ListUser struct {
+	Users []UserModel `json:"users"`
+}
+
+func GetUser(id int32) (*UserModel, error) {
+	listUser, err := queryData()
+	if err != nil {
+		return nil, err
+	}
+	for _, user := range listUser.Users {
+		if user.ID == id {
+			return &user, nil
+		}
+	}
+	return nil, fmt.Errorf("user with ID %d not found", id)
+}
+
+func UpdateUser(userID int32, name, email string) (*UserModel, error) {
+	listUser, err := queryData()
+	if err != nil {
+		return nil, err
+	}
+	found := false
+	var updatedUser *UserModel
+	for i, user := range listUser.Users {
+		if user.ID == userID {
+			listUser.Users[i].Name = name
+			listUser.Users[i].Email = email
+			found = true
+			updatedUser = &listUser.Users[i]
+			break
+		}
+	}
+	if !found {
+		return nil, fmt.Errorf("user with ID %d not found", userID)
+	}
+	updatedData, err := json.MarshalIndent(listUser, "", "  ")
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal JSON data: %v", err)
+	}
+	err = ioutil.WriteFile(PATH_DATA, updatedData, 0777)
+	if err != nil {
+		return nil, fmt.Errorf("failed to write JSON file: %v", err)
+	}
+	return updatedUser, nil
+}
+
+func UpdateUserAvatar(userID int32, avatar string) (err error) {
+	listUser, err := queryData()
+	if err != nil {
+		return err
+	}
+	for i, user := range listUser.Users {
+		if user.ID == userID {
+			listUser.Users[i].Avatar = avatar
+			break
+		}
+	}
+	updatedData, err := json.MarshalIndent(listUser, "", "  ")
+	if err != nil {
+		return fmt.Errorf("failed to marshal JSON data: %v", err)
+	}
+	err = ioutil.WriteFile(PATH_DATA, updatedData, 0777)
+	if err != nil {
+		return fmt.Errorf("failed to write JSON file: %v", err)
+	}
+	return nil
+}
+
+func queryData() (*ListUser, error) {
+	data, err := ioutil.ReadFile(PATH_DATA)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read JSON file: %v", err)
+	}
+
+	var jsonData ListUser
+	err = json.Unmarshal(data, &jsonData)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON data: %v", err)
+	}
+	return &jsonData, nil
+}
